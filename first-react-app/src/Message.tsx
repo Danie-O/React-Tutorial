8a0077a5f4f2bdd9df6d79138feefbc7fbb6e274
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-04-17 22:12:55 +0000

    Call Message Component in App.tsx

--- first-react-app/src/Message.tsx
+++ first-react-app/src/Message.tsx
@@ -2,7 +2,10 @@
 
 function Message() {
     // The below is an example of JSX code: Javascript XML
+    const name = 'Daniella';
+    if (name)
+        return <h1>Hello {name}</h1>;
     return <h1>Hello World</h1>
 }
 
-export default Message
+export default Message;
