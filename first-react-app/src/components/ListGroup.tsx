9877c7b6df6c98602f780a6d1298a95e5bb86171
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-05-04 02:51:40 +0000

    Try conditional rendering & basic event handling

--- first-react-app/src/components/ListGroup.tsx
+++ first-react-app/src/components/ListGroup.tsx
@@ -1,11 +1,26 @@
+// import { Fragment } from "react";
+import { MouseEvent } from "react";
+
 function ListGroup() {
-    return <ul className="list-group">
-    <li className="list-group-item">An item</li>
-    <li className="list-group-item">A second item</li>
-    <li className="list-group-item">A third item</li>
-    <li className="list-group-item">A fourth item</li>
-    <li className="list-group-item">And a fifth one</li>
-  </ul>;
+  let items = ['New York', 'Tokyo', 'San Francisco', 'Paris', 'London'];
+  // items = []
+
+  // Click event handler
+  const handleEvent = (event: MouseEvent) => console.log(event)
+  return (
+      <>
+        <h1>List</h1>
+        {items.length == 0 && <p>No items found</p>};
+        <ul className="list-group">
+          {items.map((item, index) => (
+            <li className="list-group-item flush" key={item} onClick={handleEvent}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </>
+  );
 }
 
-export default ListGroup
+export default ListGroup;
