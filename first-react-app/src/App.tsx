bfbc0057cda16d8e8dfbdacb7e65a8bb9852e0d6
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-05-12 19:43:40 +0000

    Update App.tsx with Props

--- first-react-app/src/App.tsx
+++ first-react-app/src/App.tsx
@@ -1,7 +1,16 @@
 import ListGroup from './components/ListGroup';
 
 function App() { 
-  return <div><ListGroup /></div>;
+  let items = ['New York', 'Tokyo', 'San Francisco', 'Paris', 'London']
+  let schools = ['Academic City', 'Ashesi', 'KNUST']
+  const handleSelectItem = (item: string) => {
+    console.log(item)
+  }
+
+  return <div>
+      <ListGroup items={items} heading='Cities' onSelectItem={handleSelectItem}/>
+      <ListGroup items={schools} heading='Schools' onSelectItem={handleSelectItem}/>
+    </div>;
 }
 
 export default App;
